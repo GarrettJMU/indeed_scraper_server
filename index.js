73f438aea7869479dcb1d1da2639e56fdf8c9c09
GarrettJMU <garretthughes3@gmail.com>
2019-11-09 12:50:44 -0800

    update response to work

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const indeed = require('indeed-scraper')
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/the-goods', function (req, res) {
+        console.log(req.query)
+
+        let queryOptions = {
+            host: 'www.indeed.com',
+            query: req.query.query,
+            city: 'San Diego, CA',
+            radius: req.query.radius || 25,
+            level: req.query.level,
+            jobType: 'fulltime',
+            maxAge: '7',
+            sort: 'date',
+            limit: 100
+        };
+
+        indeed.query(queryOptions).then(response => {
+            res.send(response)
+        })
+    })
+    .get('/', (req, res) => res.render('pages/index'))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`))
